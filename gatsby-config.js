a9c6e580fb43de91b04110ee1c66114d6da1f288
mthang1801 <mthang1801@gmail.com>
2020-12-02 21:24:20 +0700

    updating node-sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,13 +36,13 @@ module.exports = {
           },
         ],
       },
-    },  
+    },
     {
-      resolve : `gatsby-source-contentful`, 
-      options : {
-        spaceId : process.env.CONTENTFUL_SPACE_ID,
-        accessToken : process.env.CONTENTFUL_ACCESS_TOKEN
-      }
-    }
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
