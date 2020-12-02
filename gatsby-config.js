93c2ac95b79198acb923167fd7d74fa0221f6917
mthang1801 <mthang1801@gmail.com>
2020-12-02 21:14:53 +0700

    remove google-fonts from plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,18 +36,7 @@ module.exports = {
           },
         ],
       },
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Open Sans`,
-            variant: [`400`, `700`],
-          },
-        ],
-      },
-    },
+    },  
     {
       resolve : `gatsby-source-contentful`, 
       options : {
